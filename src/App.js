d0d2ac97fd6b5f2b068b2ba951271aa9352bc6b2
shrutiparadkar2004 <shrutiparadkar93@gmail.com>
2023-10-20 11:53:47 +0530

    News App Created

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { Card, Button } from "antd";
+import axios from "axios";
+import "./App.css";
+
+const { Meta } = Card;
+
+function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const loadNews = async () => {
+      const response = await axios.get(
+        "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3da128da75bb4e819bb876090635ca8f"
+      );
+      setNews(response.data.articles);
+    };
+    loadNews();
+  }, []);
+
+  console.log("news", news);
+
+  return (
+    <div className="App">
+    
+      {news &&
+        news.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              hoverable
+              style={{ width: "70%" }}
+              cover={<img alt="image" src={item.urlToImage} />}
+            >
+              <Meta title={item.title} description={item.content} />
+              <a href={item.url} target="_blank" rel="noopener noreferrer">
+                <Button type="primary" style={{ marginTop: "10px" }}>
+                  Read More
+                </Button>
+              </a>
+            </Card>
+          );
+        })}
+    </div>
+  );
+}
+
+export default App;
